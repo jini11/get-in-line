3bc465d7c573c22e7dc402b080741bcbb0f7837d
jini_11 <jieun21124@naver.com>
2022-10-03 14:59:53 +0900

    ControllerAdvice(2) - update ErrorCode

--- src/main/java/com/uno/getinline/constant/ErrorCode.java
+++ src/main/java/com/uno/getinline/constant/ErrorCode.java
@@ -10,31 +10,19 @@ import java.util.function.Predicate;
 
 @Getter
 @RequiredArgsConstructor
-public enum ErrorCode {
+public enum ErrorCode { // 에러 정의
 
-    OK(0, HttpStatus.OK, "Ok"),
+    OK(0, ErrorCategory.NORMAL, "Ok"),
 
-    BAD_REQUEST(10000, HttpStatus.BAD_REQUEST, "Bad request"),
-    SPRING_BAD_REQUEST(10001, HttpStatus.BAD_REQUEST, "Spring-detected bad request"),
-    VALIDATION_ERROR(10002, HttpStatus.BAD_REQUEST, "Validation error"),
-    NOT_FOUND(10003, HttpStatus.NOT_FOUND, "Requested resource is not found"),
+    BAD_REQUEST(10000, ErrorCategory.CLIENT_SIDE, "Bad request"),
+    SPRING_BAD_REQUEST(10001, ErrorCategory.CLIENT_SIDE, "Spring-detected bad request"),
 
-    INTERNAL_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error"),
-    SPRING_INTERNAL_ERROR(20001, HttpStatus.INTERNAL_SERVER_ERROR, "Spring-detected internal error"),
-    DATA_ACCESS_ERROR(20002, HttpStatus.INTERNAL_SERVER_ERROR, "Data access error")
+    INTERNAL_ERROR(20000, ErrorCategory.SERVER_SIDE, "Internal error"),
+    SPRING_INTERNAL_ERROR(20001, ErrorCategory.SERVER_SIDE, "Spring-detected internal error")
     ;
-//    OK(0, ErrorCategory.NORMAL, "Ok"),
-//
-//    BAD_REQUEST(10000, ErrorCategory.CLIENT_SIDE, "Bad request"),
-//    SPRING_BAD_REQUEST(10001, ErrorCategory.CLIENT_SIDE, "Spring-detected bad request"),
-//
-//    INTERNAL_ERROR(20000, ErrorCategory.SERVER_SIDE, "Internal error"),
-//    SPRING_INTERNAL_ERROR(20001, ErrorCategory.SERVER_SIDE, "Spring-detected internal error")
-//    ;
 
     private final Integer code;
-//    private final ErrorCategory errorCategory;
-    private final HttpStatus httpStatus;
+    private final ErrorCategory errorCategory;
     private final String message;
 
     public String getMessage(Exception e) {
@@ -58,5 +46,9 @@ public enum ErrorCode {
     @Override
     public String toString() {
         return String.format("%s (%d)", this.name(), this.getCode());
+    }
+
+    public enum ErrorCategory {
+        NORMAL, CLIENT_SIDE, SERVER_SIDE
     }
 }
